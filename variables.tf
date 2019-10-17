ffee3fa8ffe3f66f87560caca650cd97910944a7
santhoshkumarvs <39546013+santhoshkumarvs@users.noreply.github.com>
2019-10-17 17:18:46 +0530

    Terraform-vpc creation
    Installing Ansible,python and Apache inside the vpc  using terraform script

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "access_key" {
+    type = "string"
+    default = "AKIA3H2CWBK4SJIXDMS6"
+}
+
+variable "secret_key" {
+    type = "string"
+    default = "ZSGTNF8y66KFtPzl4C1RZXq+T9d/Cr3oAkWwtBiq"
+}
+
+variable "region" {
+    type = "string"
+    default = "us-east-1"
+}
+
+variable "instance_type" {
+    type = "string"
+    default = ""
+}
